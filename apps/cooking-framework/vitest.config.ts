aade320a61e505992277f5f60b2eb5818966376a
Marek Firlejczyk <marekfirlejczyk@Mareks-MacBook-Pro.local>
2024-07-22 18:45:23 +0200

    feat: init repo
    Iinit with nx, bun. Add simple app with nuxt

---
+++ apps/cooking-framework/vitest.config.ts
@@ -0,0 +1,30 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/apps/cooking-framework',
+
+  plugins: [vue(), nxViteTsPaths()],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  test: {
+    watch: false,
+    passWithNoTests: true,
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../coverage/apps/cooking-framework',
+      provider: 'v8',
+    },
+  },
+});
